a2318e67147b87444847a258553f339873dcf9c6
ryunkmr <nakaryumurasei@gmail.com>
2025-03-06 01:16:15 +0900

    fix

--- module.mjs
+++ module.mjs
@@ -2,16 +2,29 @@ import { ExxxConfig, Navigator } from '#exxx'
 import { ConfigKeys } from './modules/constants.mjs'
 import { join, dirname } from 'path'
 
+// ビューセットアップ
+const nav = new Navigator()
+nav.register([
+  { alias: 'setup', url: join(dir, 'views/setup.html') }
+])
+
+async function showSetup(retry) {
+  try {
+    if (retry <= 0) return
+    await nav.forward('setup')
+  } catch (error) {
+    console.error(error)
+
+    setTimeout(() => showSetup(retry), 1)
+  }
+}
+
 export default function (accessor) {
   // プラグインロード時
   accessor.listen('plugin.load', async () => {
     const dir = dirname(import.meta.url)
 
-    // ビューセットアップ
-    const nav = new Navigator()
-    nav.register([
-      { alias: 'setup', url: join(dir, 'views/setup.html') }
-    ])
+    
 
     // 設定のセットアップ
 
@@ -23,14 +36,8 @@ export default function (accessor) {
     // 保存先
     if (!conf.has(ConfigKeys.savePath)) {
       // セットアップ画面表示
-      for (let i = 0; i < 2; i++) {
-        try {
-          await nav.forward('setup')
-          i = 2
-        } catch (error) {
-          console.error(error)
-        }
-      }
+
+        showSetup(1)
     }
   })
 }
