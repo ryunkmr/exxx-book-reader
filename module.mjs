ee9c576250ac3da58babdf709689cecd0f150dab
ryunkmr <nakaryumurasei@gmail.com>
2025-03-06 01:06:09 +0900

    fix

--- module.mjs
+++ module.mjs
@@ -7,9 +7,6 @@ export default function (accessor) {
   accessor.listen('plugin.load', async () => {
     const dir = dirname(import.meta.url)
 
-    console.log(dir)
-    console.log(join(dir, 'views/setup.html'))
-
     // ビューセットアップ
     const nav = new Navigator()
     nav.register([
