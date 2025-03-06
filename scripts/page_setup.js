b08acd071d9a91e7489849dc2a7a55f02339507b
ryunkmr <nakaryumurasei@gmail.com>
2025-03-06 14:37:17 +0900

    fix

--- scripts/page_setup.js
+++ scripts/page_setup.js
@@ -10,7 +10,8 @@ async function submitSetup() {
 
   const result = await window.api.invoke('ryunkmr@exxx-book-reader:config:submit', config)
   if (result[0].success) {
-    window.location = './index.html'
+    // window.location = './index.html'
+    window.api.navigate('index')
   } else {
     toast({
       color: 'danger',
